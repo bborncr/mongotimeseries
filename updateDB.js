c51a57027899c8dc10f36709098fef1536660f3b
Bentley Born <bentley@crcibernetica.com>
2017-05-02 18:21:40 +0000

    trying to get updateDB.js to work

--- updateDB.js
+++ updateDB.js
@@ -9,30 +9,32 @@ mongoClient.connect(url, function(err, db) {
     } else {
         var mins = 0;
         var secs = 0;
-        var newValue = 50;
+        var newValue = 57;
         var collection = db.collection('timeseriesdata');
         var selector = {"timestamp_hour": "2017-10-10T10:00:00:000Z"};
-        //var updater = setInterval(updateTimeSeries, 1000);
-        updateTimeSeries();
-
-        db.close();
-
-        function updateTimeSeries() {
-            var valuesToSet = "{\"values." + mins + "." + secs + "\":" + newValue + "}";
+       //var updater = setInterval(timeSeriesUpdate, 1000);
+       timeSeriesUpdate();
+       function timeSeriesUpdate(){
+        var valuesToSet = "{\"values." + mins + "." + secs + "\":" + newValue + "}";
             //var valuesToSet = {"values.0.0": 46};
             console.log(valuesToSet);
             valuesToSet = JSON.parse(valuesToSet);
 
-        collection.updateOne(selector, {
-            $set: valuesToSet
-        }, function(err, results) {
-            console.log(results.result);
-        });
+            collection.updateOne(selector, {
+                $set: valuesToSet
+            }, function(err, results) {
+                console.log(err);
+                console.log(results.result);
+            });
             secs++;
             if(secs > 59){
                 clearInterval(updater);
                 console.log("End of Sequence!!!")
             }
-        };
-    }
-    });
+
+        }
+    };
+
+    db.close();
+
+});
